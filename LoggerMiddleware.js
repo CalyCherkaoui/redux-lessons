83b64ba1013484d0a722fce7f4a89e9e36e49a88
Houda Cherkaoui <houda.cherkaoui@gmail.com>
2021-06-24 15:12:38 -0500

    Implement Logger Midleware

--- LoggerMiddleware.js
+++ LoggerMiddleware.js
@@ -1,93 +1,64 @@
-// Import redux and logger ----------------------------
 const redux = require('redux')
 const reduxLogger = require('redux-logger')
-
-// Create  Logger -----------------------------------
+const createStore = redux.createStore
+const combineReducers = redux.combineReducers
+const applyMiddleware = redux.applyMiddleware
 const logger = reduxLogger.createLogger()
 
-// Apply Middleware
-const applyMiddleware = redux.applyMiddleware()
-
-// Actions
 const BUY_CAKE = 'BUY_CAKE'
 const BUY_ICECREAM = 'BUY_ICECREAM'
-// action creator --------------------------------------
 
-function buyCake() {
+function buyCake () {
   return {
     type: BUY_CAKE,
     info: 'First redux action'
   }
 }
 
-function buyIcecream() {
+function buyIceCream () {
   return {
-    type: BUY_ICECREAM,
-    info: 'action of buying ice cream'
+    type: BUY_ICECREAM
   }
 }
 
-// split initial state
 const initialCakeState = {
   numOfCakes: 10
 }
 
-const initialIcecreamState = {
-  numOfIcecreams: 20
+const initialIceCreamState = {
+  numOfIceCreams: 20
 }
 
-// implemnt two reducers reducer(prvious sate, action) => new sate --------------
 const cakeReducer = (state = initialCakeState, action) => {
-  switch(action.type) {
+  switch (action.type) {
     case BUY_CAKE: return {
-      ...state, // make copy of state obj and then only update numOfCake property
+      ...state,
       numOfCakes: state.numOfCakes - 1
     }
-
     default: return state
   }
 }
 
-const IcecreamReducer = (state = initialIcecreamState, action) => {
-  switch(action.type) {
+const iceCreamReducer = (state = initialIceCreamState, action) => {
+  switch (action.type) {
     case BUY_ICECREAM: return {
       ...state,
-      numOfIcecreams: state.numOfIcecreams - 1
+      numOfIceCreams: state.numOfIceCreams - 1
     }
-
     default: return state
   }
 }
-
-// Implementing the App Store ----------------------------
-const createStore = redux.createStore
-
-// 0. we combine reducers
-const combineReducers = redux.combineReducers
 
 const rootReducer = combineReducers({
   cake: cakeReducer,
-  icecream: IcecreamReducer
+  iceCream: iceCreamReducer
 })
-
-// 1. holds app's state use combined reducers and use the logger middleware
 const store = createStore(rootReducer, applyMiddleware(logger))
-
-// 2. access to state via getState method
-console.log('initial state', store.getState())
-
-// 3. Allow app to subscribe to changes in the store
-// const unsubscribe = store.subscribe(() => console.log('Updated state', store.getState()))
-const unsubscribe = store.subscribe(() => {})
-
-// 4. dispatch method to update the state 3 times
+console.log('Initial State ', store.getState())
+const unsubscribe = store.subscribe(() => { })
 store.dispatch(buyCake())
 store.dispatch(buyCake())
 store.dispatch(buyCake())
-store.dispatch(buyIcecream())
-store.dispatch(buyIcecream())
-
-// 5. to unsubscribe we use return in subscribe method
+store.dispatch(buyIceCream())
+store.dispatch(buyIceCream())
 unsubscribe()
-
-// ---------------- run : node index ---------------
